e1091cd8dc1076d098e430f4ce054c6504f60e0b
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-11-07 11:51:52 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,18 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
-if (process.env.JAWS_DB) {
-    sequelize = new Sequelize(process.env.JAWS_DB)
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(
-        
-    )
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
 }
